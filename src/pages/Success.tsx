75a586aec00fb150d79aeaa2504e3583799e86d2
AnilMandavilli594 <61509925+AnilMandavilli594@users.noreply.github.com>
2024-03-31 00:30:52 -0400

    success-page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Success.css';
 
-function Success() {
+function SuccessPage() {
   return (
-    <div>Success</div>
-  )
+    <div className="success-container">
+      <h1 className="success-title">Success!</h1>
+      <p className="success-message">You are logged in sucessfully.</p>
+    </div>
+  );
 }
 
-export default Success
+export default SuccessPage;
